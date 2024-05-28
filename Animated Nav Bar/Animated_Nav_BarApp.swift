eb534f0a71b3e44d34e753420f752b9e05f0502c
Haseeb Khan <devlpr.khan@gmail.com>
2024-05-28 19:17:44 +0500

    Initial Commit

---
+++ Animated Nav Bar/Animated_Nav_BarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animated_Nav_BarApp.swift
+//  Animated Nav Bar
+//
+//  Created by Haseeb Khan on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Animated_Nav_BarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
